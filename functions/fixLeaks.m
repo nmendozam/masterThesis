18d43f18a757d2cdd1f07e3ebb12b20372751b8a
nmendozam <mendozamejian@gmail.com>
2021-11-09 02:23:51 -0500

    Mejoras encontrando los pares desbalanceados

--- functions/fixLeaks.m
+++ functions/fixLeaks.m
@@ -32,6 +32,8 @@ model = removeRxns(model, 'steroids');
 [model, ModifiedRxns] = changeRxnMets(model, {'alpa_hs[c]'}, {'alpa_hs[c]', 'o2[c]', 'co2[c]', 'h[c]', 'pi[c]'}, 'AGPAT2', [-3, -8, 23, 35, 2]');
 [model, ModifiedRxns] = changeRxnMets(model, {'h[c]', 'h2o[c]'}, {'h[c]', 'h2o[c]'}, 'HMR_1186', [-2, 1]');
 [model, ModifiedRxns] = changeRxnMets(model, {'h[m]'}, {'h[m]'}, 'HMR_9818', [4517]');
+% [model, ModifiedRxns] = changeRxnMets(model, {'o2[c]'}, {'o2[c]'}, 'HMR_5168', [2336/4]');
+% [model, ModifiedRxns] = changeRxnMets(model, {'o2[c]'}, {'o2[c]'}, 'HMR_9818', [2336/4]');
 
 %% Test if corrected
 while true
@@ -45,13 +47,23 @@ while true
         = findMassLeaksAndSiphons(model,model.SIntMetBool,model.SIntRxnBool,modelBoundsFlag,leakParams,printLevel);
 
     %% For each leaking and siphon metabolite find a minimal cardinality leakage mode
-    leakParams.epsilon=1e-4;
-    minLeakParams.eta = getCobraSolverParams('LP', 'feasTol')*100;
-    leakParams.method='dc';
-    printLevel=1;
-    minLeakParams.monoMetMode=1;
-    [minLeakMetBool,minLeakRxnBool,minSiphonMetBool,minSiphonRxnBool,leakY,siphonY,statp,statn] =...
-        findMinimalLeakageModeMet(model,(siphonMetBool | leakMetBool),model.SIntRxnBool,modelBoundsFlag,minLeakParams,printLevel);
+    Indexes = find(siphonMetBool | leakMetBool);
+    TestMet = false(size(model.mets));
+    for i=1:length(Indexes)
+        TestMet(Indexes(i)) = true;
+
+        leakParams.epsilon=1e-4;
+        minLeakParams.eta = getCobraSolverParams('LP', 'feasTol')*100;
+        leakParams.method='dc';
+        printLevel=1;
+        minLeakParams.monoMetMode=1;
+        [minLeakMetBool,minLeakRxnBool,minSiphonMetBool,minSiphonRxnBool,leakY,siphonY,statp,statn] =...
+            findMinimalLeakageModeMet(model,TestMet,model.SIntRxnBool,modelBoundsFlag,minLeakParams,printLevel);
+        
+        if any(any(full(minLeakMetBool)))
+            break;
+        end
+    end
 
     %% Balance first metabolite
     LeakMets = model.mets(any(minLeakMetBool'));
@@ -66,56 +78,130 @@ function Table = getStoichTable(model, RxnInd)
 %
 % Syntax: Table = getStoichTable(model)
 %
-% This function assemples a table with:
+% This function assembles a table with:
 % 
 
-Table = array2table(RxnInd);
-Table.Rxns = model.rxns(RxnInd);
-Table.Formula = printRxnFormula(model, 'rxnAbbrList', Table.Rxns, 'printFlag', false);
-for i=1:length(model.Elements)
-    element = model.Elements{i};
-    balance = checkBalance(model, element, 0);
-    Table.(element) = balance(Table.RxnInd);
-end
+    Table = array2table(RxnInd);
+    Table.Rxns = model.rxns(RxnInd);
+    model = extractSubNetwork(model, Table.Rxns);
+    RxnInd = findRxnIDs(model, Table.Rxns);
 
-% Remove balanced reactions
-Table = Table(sum(Table{:, 4:end}') ~= 0, :);
+    % Remove unknown elements
+    % model.Elements = model.Elements(~ismember(model.Elements, {'X', 'R', 'FULLR'}));
+
+    % Table.Formula = printRxnFormula(model, 'rxnAbbrList', Table.Rxns, 'printFlag', false);
+    for i=1:length(model.Elements)
+        element = model.Elements{i};
+        balance = checkBalance(model, element, 0);
+        Table.(element) = balance(RxnInd);
+    end
+
+    % Remove balanced reactions
+    Table = Table(sum(Table{:, 4:end}') ~= 0, :);
 
 end
 
 function Tables = getCommonMetRxns(model, metabolite, Table)
     
     % find reactions with common metabolites
-    Table.Mets = cellfun(@(x) findMetsFromRxns(model, x), Table.Rxns, 'UniformOutput',false);
-    Table.Mets = cellfun(@(x) regexprep(x, '\[.*\]', ''), Table.Mets, 'UniformOutput', false);
-    % Remove current metabolite
+    [Table.Mets, Table.Stoichiometries]  ...
+        = cellfun(@(x) findMetsFromRxns(model, x), Table.Rxns, 'UniformOutput',false);
+    Table.Mets = cellfun(@(x) regexprep(x{1}, '\[.*\]', ''), Table.Mets, 'UniformOutput', false);
+    Table.Stoichiometries = cellfun(@(x) full(x{1}), Table.Stoichiometries, 'UniformOutput', false);
+    % Remove current metabolite compartment
     metabolite = regexprep(metabolite, '\[.*\]', '');
+    % Remove irrelevant metabolites
     Table.Mets = cellfun(@(x) x(~ismember(x, metabolite)), Table.Mets, 'UniformOutput', false);
+    Table.Stoichiometries = cellfun(@(met, stoich) ...
+        stoich(~ismember(met, metabolite)), Table.Mets, Table.Stoichiometries, 'UniformOutput', false);
+    Table.Mets = cellfun(@(x) x(~ismember(x, {'h2o'})), Table.Mets, 'UniformOutput', false);
+    Table.Stoichiometries = cellfun(@(met, stoich) ...
+        stoich(~ismember(met, {'h2o'})), Table.Mets, Table.Stoichiometries, 'UniformOutput', false);
+    Table.Mets = cellfun(@(x) x(~ismember(x, {'o2'})), Table.Mets, 'UniformOutput', false);
+    Table.Stoichiometries = cellfun(@(met, stoich) ...
+        stoich(~ismember(met, {'o2'})), Table.Mets, Table.Stoichiometries, 'UniformOutput', false);
+    Table.Mets = cellfun(@(x) x(~ismember(x, {'h'})), Table.Mets, 'UniformOutput', false);
+    Table.Stoichiometries = cellfun(@(met, stoich) ...
+        stoich(~ismember(met, {'h'})), Table.Mets, Table.Stoichiometries, 'UniformOutput', false);
+
+    Tables = {};
     
+    % Getr products and reactants
+    products = cellfun(@(met, stoich) ...
+        met(stoich > 0),Table.Mets, Table.Stoichiometries, 'UniformOutput', false);
+    reactants = cellfun(@(met, stoich) ...
+        met(stoich < 0),Table.Mets, Table.Stoichiometries, 'UniformOutput', false);
+
     % Find groups of reactions with common names
-    commonMets = cell(1, height(Table));
+    commonMets = zeros(height(Table));
     for i=1:height(Table)
-        commonMets{i} = cellfun(@(x) any(ismember(Table.Mets{i}, x)), Table.Mets);
+        commonMets(i,:) = cellfun(@(x) sum(ismember(reactants{i}, x)), products);
     end
 
-    m = cat(3, commonMets{:});
-    m = reshape(m, [], size(m, 3)).';
-    rxn_groups = unique(m, 'rows');
+    while true
+        [value, xindex] = max(commonMets);
+        [value, yindex] = max(value);
+        if any(sum(commonMets == value) > 1)
+            break;
+        end
+        xindex = xindex(yindex);
+        fprintf('Value %d [%d,%d]\r\n', value, xindex, yindex)
+        Tables{end + 1} = Table([xindex, yindex],:);
+        commonMets([xindex, yindex], :) = 0;
+        commonMets(:, [xindex, yindex]) = 0;
+    end
 
+    rxn_groups = unique(commonMets >= 1, 'rows');
+    rxn_groups = rxn_groups(sum(rxn_groups, 2) == 2, :);
+
+    % if isempty(rxn_groups)
+    %     commonMets = zeros(height(Table));
+    %     for i=1:height(Table)
+    %         commonMets(i,:) = cellfun(@(x) sum(ismember(Table.Mets{i}, x)), Table.Mets);
+    %     end
+    %     rxn_groups = unique(commonMets >= 1, 'rows');
+    % end
+    
     % Remove reactions that don't have common metabolites
-    Tables = cell(1, size(rxn_groups, 1));
     for i=1:size(rxn_groups, 1)
-        Tables{i} = Table(rxn_groups(i,:), :);
+        Tables{end + 1} = Table(rxn_groups(i,:), :);
     end
+
 
 end
 
 function weights = get_reactions_weights(model, Table)
-    Table.Mets = cellfun(@(x) findMetsFromRxns(model, x), Table.Rxns, 'UniformOutput',false);
-    Table.Mets = cellfun(@(x) regexprep(x, '\[.*\]', ''), Table.Mets, 'UniformOutput', false);
+    % Get metabololites and stoichiometries
+    [Table.Mets, Table.Stoichiometries]  ...
+        = cellfun(@(x) findMetsFromRxns(model, x), Table.Rxns, 'UniformOutput',false);
+    Table.Mets = cellfun(@(x) regexprep(x{1}, '\[.*\]', ''), Table.Mets, 'UniformOutput', false);
+    Table.Stoichiometries = cellfun(@(x) full(x{1}), Table.Stoichiometries, 'UniformOutput', false);
+
+    % Split rxns in by reactants and products imbalance
+    imbalance = sum(table2array(Table(:,4:end-2)), 2);
+    reactants = imbalance < 0;
+    products = imbalance > 0;
+
+    % Get reactant and product metabolites
+    Table.Mets(reactants) = cellfun(@(met, stoich) ...
+        met(stoich < 0),Table.Mets(reactants), Table.Stoichiometries(reactants), 'UniformOutput', false);
+    Table.Mets(products) = cellfun(@(met, stoich) ...
+        met(stoich > 0),Table.Mets(products), Table.Stoichiometries(products), 'UniformOutput', false);
+    Table.Stoichiometries(reactants) = cellfun(@(stoich) ...
+        stoich(stoich < 0), Table.Stoichiometries(reactants), 'UniformOutput', false);
+    Table.Stoichiometries(products) = cellfun(@(stoich) ...
+        stoich(stoich > 0), Table.Stoichiometries(products), 'UniformOutput', false);
+
+    % Keep only common metabolites between reactants and products
     for i=1:height(Table)
-        commonMets{i} = ismember(Table.Mets{i}, cat(1, Table.Mets{Table.H < 0}));
-        Table.Mets{i}(x)
+
+        commonReactants = ismember(Table.Mets{i}, cat(1, Table.Mets{reactants}));
+        Table.Stoichiometries{i} = Table.Stoichiometries{i}(commonReactants);
+        Table.Mets{i} = Table.Mets{i}(commonReactants);
+
+        commonPoducts = ismember(Table.Mets{i}, cat(1, Table.Mets{products}));
+        Table.Stoichiometries{i} = Table.Stoichiometries{i}(commonPoducts);
+        Table.Mets{i} = Table.Mets{i}(commonPoducts);
     end
 end
 
@@ -135,28 +221,41 @@ function model = balance(model, metabolite, Table)
             balanced = sum(pairs') == 0;
             disRxns(m,:) = ~ismember(Table{:, MetElemet(m)}, pairs(balanced, :));
         end
-        Table = Table(any(disRxns), :);
+        if length(MetElemet) > 1
+            Table = Table(any(disRxns), :);
+        else
+            Table = Table(disRxns >= 1, :);
+        end
 
+        if height(Table) <= 1
+            break;
+        end
+        
         % Once we have found the disblanced reactions
         % Fix the stoichiometry finding a midle point between those
         for m=1:length(MetElemet)
+            % Find the metabolite candidate
+            met_compartment = regexp(metabolite, '\[.*\]', 'match');
+            met_no_num = regexprep(model.metFormulas, '\d', '');
+            met_candidates = model.mets(ismember(met_no_num, MetElemet(m)));
+            met_candidate = met_candidates{contains(met_candidates, met_compartment{1}{1})};
+            met_candidateInd = findMetIDs(model, met_candidate);
+            [candidate_Ematrix, ~] = getElementalComposition(model.metFormulas{met_candidateInd}, model.Elements);
+            candidate_Ematrix = candidate_Ematrix(candidate_Ematrix > 0);
+
             stoich_change = zeros(height(Table), 1);
-            balance = -difference(m)/(2 * Ematrix(m));
+            balance = -difference(m)/(2 * candidate_Ematrix);
             % Balance values for each side of the reaction
             MetColum = Table.(MetElemet{m});
             stoich_change(MetColum > 0) = balance * MetColum(MetColum > 0) / sum(MetColum(MetColum > 0));
             stoich_change(MetColum < 0) = balance * MetColum(MetColum < 0) / sum(MetColum(MetColum < 0));
-            stoich_change = model.S(MetInd, Table.RxnInd) + stoich_change';
+            stoich_change = model.S(met_candidateInd, Table.RxnInd) + stoich_change';
             
-            % Find the metabolite candidate
-            met_compartment = regexp(metabolite, '\[.*\]', 'match');
-            met_candidates = model.mets(ismember(model.metFormulas, MetElemet(1)));
-            met_candidate = met_candidates{contains(met_candidates, met_compartment{1}{1})};
 
             % Change the stoichiometry in the model
             [model, ModifiedRxns] = changeRxnMets(model, {met_candidate}, {met_candidate}, Table.Rxns, stoich_change);
             for i=1:height(Table)
-                fprintf('Changing %s with %d %d\n', Table.Rxns{i}, stoich_change(i), met_candidate)
+                fprintf('Changing %s with %d %s\n', Table.Rxns{i}, stoich_change(i), met_candidate)
             end
         end
 
@@ -197,35 +296,24 @@ while true
     else
         break;
     end
-    if (strcmp(metabolite, 'h[m]') && sum(minSiphonRxnBool) >= 32)
-        disp('pare aqui')
-    end
 
     % Generate table with stats of the leaking reactions
-    Table = getStoichTable(model, RxnInd);
+    Table = getStoichTable(model, RxnInd)
+    % Remove balanced reactions
+    % Table = Table(sum(Table(5:end)),:)
     if height(Table) > 1
         Tables = getCommonMetRxns(model, metabolite, Table);
         for i=1:length(Tables)
             model = balance(model, metabolite, Tables{i});
+            % model = balance(model, {'h[c]'}, Tables{i});
         end
     else
         model = balance(model, metabolite, Table);
     end
-    % If this are balanced use the sink
-%     if ~any(sum(Table{:, MetElemet}))
-%         leakInd = find(minSiphonMetBool(MetInd,:));
-%         RxnInd = find(minSiphonRxnBool(:, leakInd));
-%         Table = getStoichTable(model, RxnInd);
-%     end
-%     if height(Table) == 1
-%         mets = findMetsFromRxns(model, Table.Rxns);
-%         Form = model.metFormulas(findMetIDs(model, mets));
-%         Rs = array2table(sum(stoich(Form)')');
-%         Rs.Formulas = Form;
-%         Rs.Names = mets
-%     else
-        
-%     end
+    % RxnBool = false(size(model.rxns));
+    % RxnBool(Table.RxnInd) = true;
+    % [minLeakMetBool,minLeakRxnBool,minSiphonMetBool,minSiphonRxnBool, ~, ~, ~, ~] =...
+    %     findMinimalLeakageModeMet(model, TestMet, RxnBool,modelBoundsFlag,minLeakParams,printLevel);
 
 end
 
